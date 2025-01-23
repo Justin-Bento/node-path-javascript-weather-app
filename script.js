6078254403cd01452c929aea3ac1a1984c6c4696
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-01-23 11:26:54 -0400

    Created a fetch command that returns todo post. from {JSON} Placeholder

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 const myData = document.querySelector(".fetchData");
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
