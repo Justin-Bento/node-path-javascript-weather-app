efb45a6b5dfdfc3842d9422277d68016ff6dfa4c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-01-25 11:12:43 -0400

    replaced script.js with index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 // index.js
 import "./style.css";
 import { greeting } from "./greeting.js";
+const myData = document.querySelector(".fetchData");
 
 console.log(greeting);
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+  });
