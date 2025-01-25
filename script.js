36ee2f17d95a9e164272a19f2387cd0290b3749d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-01-25 10:09:40 -0400

    Implemented heading class and applied general styles for the class.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((json) => {
     console.log(json);
     let node = document.createElement("h2");
+    node.setAttribute("class", "heading");
     node.textContent = json.title;
     myData.appendChild(node);
   });
