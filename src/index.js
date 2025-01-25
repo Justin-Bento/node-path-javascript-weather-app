af567252f997ae3a2b5e7e2344bea096f32cc748
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-01-25 12:48:14 -0400

    Created multpule sections inside

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import "./style.css";
 import { greeting } from "./greeting.js";
 
 const node = document.querySelector(".content");
+const section = document.createElement("section");
+node.appendChild(section); // Append the heading to the node
+
 const heading = document.createElement("h1");
 heading.textContent = greeting;
-node.appendChild(heading); // Append the heading to the node
+section.appendChild(heading);
