514df1a206f3156eb734288e96321d5eee61a02f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-01-25 10:05:27 -0400

    Created a new h2 element and outputed the title of the content.

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ const myData = document.querySelector(".fetchData");
 
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => {
+    console.log(json);
+    let node = document.createElement("h2");
+    node.textContent = json.title;
+    myData.appendChild(node);
+  });
