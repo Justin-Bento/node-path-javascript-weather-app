26b045e2d1076db47a6059b4e8e419bfae1edf3e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-01-27 14:32:51 -0400

    Utilized fetch async await function to grab data from the api

--- src/index.js
+++ src/index.js
@@ -9,3 +9,20 @@ node.appendChild(section); // Append the heading to the node
 const heading = document.createElement("h1");
 heading.textContent = greeting;
 section.appendChild(heading);
+
+async function getData() {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=${process.env.WEATHER_API_KEY}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getData();
