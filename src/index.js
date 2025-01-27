5436e761a00aeee786475cee24945b97365f53c8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-01-27 15:01:04 -0400

    Fetched data from the api and displayed the reolved adress.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,19 @@ import { greeting } from "./greeting.js";
 const node = document.querySelector(".content");
 const section = document.createElement("section");
 section.setAttribute("class", ["container"]);
-node.appendChild(section); // Append the heading to the node
-
-const heading = document.createElement("h1");
-heading.textContent = greeting;
-section.appendChild(heading);
+node.appendChild(section);
 
 async function getData() {
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=${process.env.WEATHER_API_KEY}`;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=2M3RGP5QK2USQD7ETY5S6RA6P`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-
     const json = await response.json();
-    console.log(json);
+    const heading = document.createElement("h1");
+    heading.textContent = json.resolvedAddress;
+    section.appendChild(heading);
   } catch (error) {
     console.error(error.message);
   }
