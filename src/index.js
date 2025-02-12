495f5a6b7d27c7591050c7757c48a575352fb97c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-02-12 13:40:27 -0500

    Updated fetch with new key

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./style.css";
 
+async function getData() {
+  //
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London?unitGroup=metric&key=${process.env.WEATHER_API_KEY}&contentType=json`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
+getData();
