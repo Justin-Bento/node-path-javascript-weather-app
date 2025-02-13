e456b6c502d8f5bb50322e7b220b9fd3519053ff
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-02-13 15:22:09 -0500

    Query selected a class name called forcastInformation to help call the list.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const locationInfromation = document.querySelector(".country-address");
 const currentWeatherConditions = document.querySelector(
   ".curent-weather-conditions"
 );
+// const forcastInformation = document.querySelector(".forcast-information");
 
 async function getData() {
   //
